0bc976606364f3e4f638729614fc8b54a2f84ebb
Martin Abente Lahaye <tch@sugarlabs.org>
2014-10-09 10:11:52 -0300

    Add Log to service
    Signed-off-by: Martin Abente Lahaye <tch@sugarlabs.org>

--- src/org/OneEducation/HarvestClient/HarvestService.java
+++ src/org/OneEducation/HarvestClient/HarvestService.java
@@ -3,16 +3,28 @@ package org.OneEducation.HarvestClient;
 import android.os.IBinder;
 import android.app.Service;
 import android.content.Intent;
+import android.util.Log;
 
 public class HarvestService extends Service {
 
     @Override
+    public void onCreate() {
+        Log.i("HarvestService", "is created.");
+    }
+
+    @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("HarvestService", "is running.");
         return Service.START_NOT_STICKY;
     }
 
     @Override
     public IBinder onBind(Intent intent) {
       return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i("HarvestService", "is destroyed.");
     }
 }
