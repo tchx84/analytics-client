7d640590ec590b8ce1c5edf1f3ff296a12beb8d3
Martin Abente Lahaye <tch@sugarlabs.org>
2014-10-09 10:58:01 -0300

    Call the service in regular intervals
    Signed-off-by: Martin Abente Lahaye <tch@sugarlabs.org>

--- src/org/OneEducation/HarvestClient/HarvestReceiver.java
+++ src/org/OneEducation/HarvestClient/HarvestReceiver.java
@@ -1,18 +1,33 @@
 package org.OneEducation.HarvestClient;
 
+import java.util.Calendar;
+
 import android.content.Intent;
 import android.content.Context;
 import android.content.BroadcastReceiver;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 
 import org.OneEducation.HarvestClient.HarvestService;
 
 public class HarvestReceiver extends BroadcastReceiver
 {
+    private int interval = 30*1000;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            Intent starterIntent = new Intent(context, HarvestService.class);
-            context.startService(starterIntent);
+            prepareService(context);
         }
+    }
+
+    private void prepareService(Context context) {
+        Intent intent = new Intent(context, HarvestService.class);
+        PendingIntent pending = PendingIntent.getService(context, 0, intent, 0);
+
+        Calendar calendar = Calendar.getInstance();
+
+        AlarmManager alarm = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        alarm.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), interval, pending);
     }
 }
